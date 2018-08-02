96146f036b2d0feb606201c1320e36714766031f
zjkang <zjkang@smail.nju.edu.cn>
2018-08-02 16:04:24 +0800

    2b-redict

--- hello.py
+++ hello.py
@@ -2,13 +2,26 @@ from flask import Flask
 from flask import request
 from flask import redirect
 from flask import render_template
+from flask import session
+from flask import url_for
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
 Bootstrap(app)
-@app.route('/')
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?',validators=[Required()])
+    submit = SubmitField('Submit')
+
 @app.route('/user/<name>')
-def index(name):
+def user(name):
     user_agent = request.headers.get('Host')
     #return '<h1>your browser is %s</h1>' % user_agent
     #return redirect('http://www.baidu.com')
@@ -16,6 +29,15 @@ def index(name):
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'),404
+
+@app.route('/',methods=['GET','POST'])
+def idnex():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name  = form.name.data
+        form.name.data = ''
+    return render_template('index.html',name = name,form = form)
 
 if __name__ == '__main__':
     app.run()
