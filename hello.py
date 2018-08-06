483a91de8aff5f178752e7b41e2227e270aaf1e7
zjkang <zjkang@smail.nju.edu.cn>
2018-08-06 16:15:28 +0800

    database

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask import request
 from flask import redirect
@@ -9,12 +10,40 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+basedir = os.path.abspath(os.path.dirname(__file__))            #获取当前文件所在路径
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost:5432/xuemc_db'
+#app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-Bootstrap(app)
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
+class Role(db.Model):
+    __tablename__ = "roles"
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(64),unique = True)
+    users = db.relationship(u'User',backref ='role',lazy="dynamic")
+
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(64),unique=True,index=True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 class NameForm(FlaskForm):
@@ -35,12 +64,17 @@ def page_not_found(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('The name has changed!')
-        session['name'] = form.name.data
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user=User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name']=form.name.data
+        form.name.data=''
         return redirect(url_for('index'))
-    return render_template('index.html',name = session.get('name'),form = form)
+    return render_template('index.html',name = session.get('name'),form = form,known = session.get('known',False))
 
 if __name__ == '__main__':
     app.run()
